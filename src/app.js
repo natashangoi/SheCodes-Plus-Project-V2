7127aead5c8218b8b05d747bda98db005edcf5a2
Natasha <natashangoi@gmail.com>
2021-06-30 19:12:38 +1200

    added display temperature function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
+function displayTemperature(response) {
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city")
+    let descriptionElement = document.querySelector("#description")
+    temperatureElement.innerHTML = Math.round(response.data.main.temp)
+    cityElement.innerHTML = (response.data.name)
+}
+
 let apiKey = "f9f0439a96ed3cf7c71b3f3742b80bfa"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
 
-console.log(apiUrl)
+axios.get(apiUrl).then(displayTemperature)
