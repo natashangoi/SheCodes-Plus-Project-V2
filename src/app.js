fb4dea0cfe345e206ad5c7e7e0451774c105cb33
Natasha <natashangoi@gmail.com>
2021-06-30 19:28:08 +1200

    updated weather function

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
-function displayTemperature(response) {
+function displayWeather(response) {
     console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city")
-    let descriptionElement = document.querySelector("#description")
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp)
-    cityElement.innerHTML = (response.data.name)
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
 }
 
 let apiKey = "f9f0439a96ed3cf7c71b3f3742b80bfa"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`
 
-axios.get(apiUrl).then(displayTemperature)
+axios.get(apiUrl).then(displayWeather)
